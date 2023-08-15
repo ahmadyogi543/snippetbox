4375dbb1c4cc41f561f7fa9a4707c053952c6325
Ahmad Yogi <ahmadyogi543@gmail.com>
2023-08-15 14:04:23 +0800

    chore: add testing for snippetView handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -16,3 +16,59 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApp(t)
+	server := newTestServer(t, app.routes())
+	defer server.Close()
+
+	tests := []struct {
+		name         string
+		urlPath      string
+		expectedCode int
+		expectedBody string
+	}{
+		{
+			name:         "Valid ID",
+			urlPath:      "/snippet/view/1",
+			expectedCode: http.StatusOK,
+			expectedBody: "This is a content inside the mock snippet.",
+		},
+		{
+			name:         "Non-existent ID",
+			urlPath:      "/snippet/view/1000",
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "Negative ID",
+			urlPath:      "/snippet/view/-1",
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "Decimal ID",
+			urlPath:      "/snippet/view/1.45",
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "String ID",
+			urlPath:      "/snippet/view/abc",
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "Empty ID",
+			urlPath:      "/snippet/view/",
+			expectedCode: http.StatusNotFound,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			code, _, body := server.get(t, test.urlPath)
+
+			assert.Equal(t, code, test.expectedCode)
+			if test.expectedBody != "" {
+				assert.StringContaints(t, body, test.expectedBody)
+			}
+		})
+	}
+}
--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,22 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("expected %v; got %v instead", expected, actual)
+	}
+}
+
+func StringContaints(t *testing.T, actual, expected string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expected) {
+		t.Errorf("expected to contain %v; got %v instead", expected, actual)
 	}
 }
